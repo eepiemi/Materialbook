bf8ad29fa72ed549bc1b3005b0c49126b55bbb6b
Emi :ɜ <thehaziemail@gmail.com>
2025-09-11 05:41:02 +0200

    Remove unneeded dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.datastore.preferences)
     testImplementation(libs.junit)
