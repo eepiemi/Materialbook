24469a1676efd77c9bb238d0c94df7931415b0a5
Emi :ɜ <thehaziemail@gmail.com>
2026-03-22 17:53:14 +0100

    Tweak settings gear icon and color

--- app/src/main/res/raw/scripts.js
+++ app/src/main/res/raw/scripts.js
@@ -254,13 +254,13 @@ observer.observe(document.body, { childList: true, subtree: true });
   onReady(() => {
     const BUTTON_ID = 'custom-settings-btn';
     const ICON_SVG = `
-      <svg width="28" height="28" fill="%FILL%" viewBox="0 0 24 24">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M9.586 2.586A2 2 0 0 1 11 2h2a2 2 0 0 1 2 2v.089l.473.196.063-.063a2.002 2.002 0 0 1 2.828 0l1.414 1.414a2 2 0 0 1 0 2.827l-.063.064.196.473H20a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-.089l-.196.473.063.063a2.002 2.002 0 0 1 0 2.828l-1.414 1.414a2 2 0 0 1-2.828 0l-.063-.063-.473.196V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.089l-.473-.196-.063.063a2.002 2.002 0 0 1-2.828 0l-1.414-1.414a2 2 0 0 1 0-2.827l.063-.064L4.089 15H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h.09l.195-.473-.063-.063a2 2 0 0 1 0-2.828l1.414-1.414a2 2 0 0 1 2.827 0l.064.063L9 4.089V4a2 2 0 0 1 .586-1.414ZM8 12a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z"/>
-      </svg>`;
+        <svg width="28" height="28" viewBox="0 -960 960 960"  fill="%FILL%">
+            <path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm112-260q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Z"/>
+        </svg>`;
 
     const getFillColor = () => {
       const color = document.querySelector('meta[name="theme-color"]')?.content?.toLowerCase();
-      return color === '#ffffff' ? '#242526' : '#d0d0d0';
+      return color === '#ffffff' ? '#080809' : '#e4e6eb';
     };
 
     const updateButtonColor = () => {
